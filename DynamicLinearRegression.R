33281206161a8f32dac47db8a92345c9c2193007
ArieFridrich <ari.levtov@gmail.com>
2023-10-27 16:13:40 +0200

    geom_segment r2 working optimizing

--- DynamicLinearRegression.R
+++ DynamicLinearRegression.R
@@ -4,8 +4,8 @@ library(shiny)
 
 HW <-tibble(
   id = c("Lahav", "Ben", "Lia", "Haari", "Maya", "Oren", "Arie", "Lili", "Michal", "Or"),
-  height = c(151, 174, 145, 186, 128, 136, 179, 163, 152, 134),
-  weight = c(63, 81, 60, 91, 47, 57, 76, 72, 62, 52)
+  height = c(18, 8, 5, 10, 3, 20, 16, 11, 4, 17),
+  weight = c(20, 6, 8, 12, 2, 14, 18, 10, 4, 16)
 )
 
 
@@ -15,7 +15,7 @@ HW <-tibble(
 
 ui <- fluidPage(
   sliderInput("y_intercept", "Y-Intercept", min = -20, max = 10, step = 0.1, value = 1),
-  sliderInput("slope", "Slope", min = 0, max = 1, step = 0.01, value = 0.5),
+  sliderInput("slope", "Slope", min = -1, max = 1, step = 0.01, value = 0.5),
   plotOutput("plot")
 )
 
@@ -28,8 +28,8 @@ server <- function(input, output, session) {
     # Create a ggplot with a straight line
     p <- ggplot(df, aes(x_hw, y_hw)) +
       geom_line(aes(color = "red")) +
-      xlim(100, 179) +
-      ylim(30, 91) +
+      xlim(0, 20) +
+      ylim(0, 20) +
       geom_point(data = HW, mapping = aes(x = height, y = weight)) +
       labs(x = "height", y = "weight") +
       geom_segment(mapping = aes(x = x_hw, y = data_y, xend = x_hw, yend = y_hw))
@@ -37,11 +37,5 @@ server <- function(input, output, session) {
   })
 }
 
-
-
 shinyApp(ui, server)
-
-
-
-
 
